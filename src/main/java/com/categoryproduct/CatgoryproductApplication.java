bd64dc5f9bfef5f84782c8241de6321817afed77
= <=>
2024-12-14 14:11:23 +0530

    initial commit

---
+++ src/main/java/com/categoryproduct/CatgoryproductApplication.java
@@ -0,0 +1,15 @@
+package com.categoryproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatgoryproductApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(CatgoryproductApplication.class, args);
+		System.out.println("application started");
+	}
+
+}
