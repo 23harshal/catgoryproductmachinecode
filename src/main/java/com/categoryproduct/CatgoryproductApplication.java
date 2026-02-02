8eedd178e49abf13423ce46266497039d043e6f6
23harshal <hh234pp@gmail.com>
2026-02-02 12:17:47 +0530

    comment added

--- src/main/java/com/categoryproduct/CatgoryproductApplication.java
+++ src/main/java/com/categoryproduct/CatgoryproductApplication.java
@@ -10,7 +10,7 @@ public class CatgoryproductApplication {
 	public static void main(String[] args)
 	{
 		SpringApplication.run(CatgoryproductApplication.class, args);
-		System.out.println("application started with jenkins and docker test......");
+		System.out.println(" started with jenkins and docker test......");
 	}
 
 }
